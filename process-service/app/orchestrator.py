aaca3592ed86cbdee64bc43400b7f11a7732195e
Moshe <moshe@example.com>
2026-04-27 16:41:33 +0300

    feat: build basic for server

--- process-service/app/orchestrator.py
+++ process-service/app/orchestrator.py
@@ -1,8 +1,66 @@
 import logging 
+import aiofiles
+from fastapi import UploadFile
+from pymavlink.DFReader import DFReader_binary
+import time
+import os 
+import tempfile
+import json 
 
 
 class Orchestrator():
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+    
+    async def write_new_file_to_disc(self, file: UploadFile, temp_file: str):
+        try:
+            import time
+            start = time.perf_counter()
+            async with aiofiles.open(temp_file,'ab') as f:
+                    context = await file.read()
+                    await f.write(context)
+            return time.perf_counter() - start 
+        except Exception:
+            self.logger.error('failed write new file to disc',exc_info=True)
+            raise
 
-
+    def _process_file(self, fileno:int):
+        tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".jsonl", mode="w", encoding="utf-8")
+        mav = None
+        try:
+            start_time = time.perf_counter()
+            duplicated_fd = os.dup(fileno)
+            mav = DFReader_binary(duplicated_fd)
+            count = 0
+            while True :
+                msg_raw = mav.recv_match(blocking=False,type=['GPS'])
+                if msg_raw is None:
+                    break
+                original_msg :dict  = msg_raw.to_dict()
+                if original_msg['I'] == 1 :
+                    keys_msg_to_keep = {'TimeUS', 'Status', 'Lat','Lng','Alt','Spd'} 
+                    new_dict = {key: original_msg.get(key,None) for key in keys_msg_to_keep}
+                    tmp_file.write(json.dumps(new_dict) + "\n")
+                count +=1
+            duration = time.perf_counter() - start_time
+            tmp_file.close()
+            mav.close() 
+            
+            return {
+                "file_path": tmp_file.name,
+                "count": count, 
+                "duration_sec": f"({int(duration//60)} min {duration%60:.2f} sec)"
+            }
+        
+        except Exception as e: 
+            if tmp_file:
+                tmp_file.close()
+            if os.path.exists(tmp_file.name):
+                os.remove(tmp_file.name)
+            if mav:
+                mav.close()
+            return {"error": str(e)}
+    
+    async def process(self,fileno:int):
+        import asyncio
+        return await asyncio.to_thread(self._process_file, fileno)
--- process-service/app/routes.py
+++ process-service/app/routes.py
@@ -1,28 +1,74 @@
-from fastapi import APIRouter, Depends, Request, UploadFile, File, HTTPException
-import aiofiles
+from fastapi import APIRouter, Depends, Request, UploadFile, File, HTTPException, BackgroundTasks
+from fastapi.responses import FileResponse
 import logging 
-import mmap 
+import os
+from .orchestrator import Orchestrator
+from .schemas import ClientResponse, StatusResult
 
 logger = logging.getLogger(__name__)
-
 router = APIRouter()
 
-def get_manager(request:Request):
-        return request.app.state.manager
+def get_manager(request: Request):
+    return request.app.state.manager
+
+def remove_temp_file(path: str):
+    try:
+        if path and os.path.exists(path):
+            os.remove(path)
+            logger.info(f"Deleted temp file: {path}")
+    except Exception as e:
+        logger.error(f"Failed to delete temp file {path}: {e}")
 
 @router.post('/process', status_code=200)
-async def process_bin_files(file : UploadFile = File(...)):
-        try:
-                temp_file = f'temp_{file.filename}'
-                async with aiofiles.open(temp_file,'ab') as f:
-                        context = await file.read()
-                        await f.write(context)
-                        print(context[:256])
-                        
-                return {'status':';ggg'} 
-        except Exception:
-                raise HTTPException(status_code=401)
+async def process_bin_files(
+    background_tasks: BackgroundTasks, 
+    file: UploadFile = File(...), 
+    manager: Orchestrator = Depends(get_manager)
+):
+    try:
+        if not is_bin_file(file.filename):
+            raise HTTPException(status_code=401, detail='only .bin format')
+        
+        result = await manager.process(file.file.fileno())
+        
+        if "error" in result:
+            raise HTTPException(status_code=500, detail=result["error"])
+        
+        file_path = result.get("file_path")
+        count = result.get("count", 0)
+        duration = result.get("duration_sec", "unknown")
+        
+        logger.info(f"File processed successfully | Total messages: {count} | Time: {duration}")
+        
+        background_tasks.add_task(remove_temp_file, file_path)
+        
+        response = FileResponse(
+            path=file_path, 
+            media_type="application/x-jsonlines", 
+            filename="gps_filtered_data.jsonl"
+        )
+        
+        # Headers
+        response.headers["X-Total-Count"] = str(count)
+        response.headers["X-Process-Duration"] = str(duration)
+        
+        return response
+        
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.error(f"Server failed: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail=f'server failed: {str(e)}')
+    
+@router.get('/result/{task_id}', status_code=200)
+async def get_task_id(task_id: int):
+    pass
 
-@router.get('/result{task_id}', status_code=200)
-async def get_task_id(task_id:int):
-        pass
+def is_bin_file(file_name: str):
+    try:
+        if file_name.endswith('.bin'):
+            return True
+        return False
+    except Exception:
+        logger.error('checking end file name failed', exc_info=True)
+        return False
