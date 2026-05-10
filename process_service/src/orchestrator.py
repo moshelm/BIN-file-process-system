d5cd062283600ac7758ad7b66f642bc01856f224
Moshe <moshe@example.com>
2026-05-10 13:33:24 +0300

    feat: adding function do_all_parser

--- process_service/src/orchestrator.py
+++ process_service/src/orchestrator.py
@@ -3,6 +3,7 @@ from shared.logger_config import get_logger
 from shared.schemas import ParseResult
 from process_service.src.parsers.pymavlink_parser.process import PymavlinkParser
 from process_service.src.parsers.multiprocess_parser.multi_process import ParseMultiprocess
+from process_service.src.parsers.multiprocess_parser.multi_process import ArduPilotParser
 from process_service.src.parsers.pure_py_parser import PurePythonParser
 
 logger = get_logger(__name__)
@@ -12,6 +13,15 @@ class Orchestrator:
         self.pymavlink_parser = PymavlinkParser()
         self.multiprocess_parser = ParseMultiprocess()
         self.python_parser = PurePythonParser()
+        self.ardupilot = ArduPilotParser()
+        
+    async def do_all_parsers(self, file_path:str):
+        res = await self.parser_pure_python(file_path)
+        yield res
+        res = await self.parser_file_multiprocess(file_path)
+        yield res 
+        res = await self.parser_all_file_mavlink(file_path)
+        yield res
 
     async def parser_gps_messages(self, file_path:str) -> ParseResult:
         return await asyncio.to_thread(self.pymavlink_parser.parse_file_by_only_GPS_Messages, file_path)
