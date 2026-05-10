adafaf7fc3e1b601bb1c1a530bdd1be1dd70de04
Moshe <moshe@example.com>
2026-05-10 13:40:07 +0300

    fix: to handle parsers information only

--- process_service/src/api/process_route.py
+++ process_service/src/api/process_route.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from fastapi import (
     APIRouter,
-    BackgroundTasks,
     Depends,
     File,
     HTTPException,
@@ -10,10 +9,11 @@ from fastapi import (
 )
 from shared.logger_config import get_logger
 from shared.schemas import ParseResult, ParseStatus
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from process_service.src.orchestrator import Orchestrator
 from process_service.src.utils.helper import is_bin_file
 from process_service.src.utils.file_handling import create_and_write_new_file_to_disc_async, remove_temp_file
+
 
 logger = get_logger(__name__)
 router = APIRouter()
@@ -21,56 +21,29 @@ router = APIRouter()
 
 def get_manager(request: Request):
     return request.app.state.manager
-    
-def headers_handling(response: FileResponse, count: int, duration: str):
-    response.headers["X-Total-Count"] = str(count)
-    response.headers["X-Process-Duration"] = str(duration)
 
-@router.post("/process_gps_messages", status_code=200)
-async def process_bin_files(
-    background_tasks: BackgroundTasks,
-    file: UploadFile = File(...),
-    manager: Orchestrator = Depends(get_manager),
-):
+async def stream_parsed_objects(manager:Orchestrator, file_path: str):
     try:
-        if not is_bin_file(file.filename):
-            raise HTTPException(status_code=401, detail="only .bin format")
-
-        temp_file_path = str(Path(f"/tmp/{file.filename}"))
+        async for parse_file_result in manager.do_all_parsers(file_path):
+       
+            if parse_file_result is None or parse_file_result.status == ParseStatus.FAILED: 
+                logger.error('Failed to process file with one of the parsers')
+                continue
         
-        await create_and_write_new_file_to_disc_async(file, temp_file_path)
-        
-        parse_gps_result : ParseResult = await manager.parser_gps_messages()
-
-        if parse_gps_result.status == ParseStatus.FAILED: 
-            raise HTTPException(status_code=500, detail='failed process this file')
-        
-        logger.info(f"File processed successfully | Total messages: {parse_gps_result.count} | Time: {parse_gps_result.duration}")
-        file_path = parse_gps_result.json_file_result_name
-        
-        response = None
-        
-        if file_path is None:
-            response = parse_gps_result
-        else:
-            background_tasks.add_task(remove_temp_file, file_path)
-            background_tasks.add_task(remove_temp_file, temp_file_path)
-
-            response = FileResponse(
-                path=file_path,
-                media_type="application/x-jsonlines",
-                filename="gps_filtered_data.jsonl",
-            )
-        headers_handling(response, parse_gps_result.count, parse_gps_result.duration)
-        return response
-    
+            logger.info(f"File processed successfully | Total messages: {parse_file_result.count} | Time: {parse_file_result.duration}")
+            res =f"{parse_file_result.model_dump_json()}\n"
+            logger.info(res)
+            yield res
+                
     except Exception as e:
-        logger.error(f"Server failed: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="server failed")
+        logger.error(f"Error during streaming: {e}", exc_info=True)
+    finally:
+        remove_temp_file(file_path)
+        logger.info(f"Temporary file {file_path} removed.")
+
 
 @router.post("/process_file", status_code=200)
 async def process_bin_files(
-    background_tasks: BackgroundTasks,
     file: UploadFile = File(...),
     manager: Orchestrator = Depends(get_manager),
 ):
@@ -82,24 +55,15 @@ async def process_bin_files(
         
         await create_and_write_new_file_to_disc_async(file, temp_file_path)
 
-        # await manager.parser_file_multiprocess()
-        # parse_file_result = await manager.parser_all_file_mavlink()
-        parse_file_result : ParseResult = await manager.parser_pure_python()
-        if parse_file_result is None or parse_file_result.status == ParseStatus.FAILED: 
-            raise HTTPException(status_code=500, detail='failed process this file')
-        
-        logger.info(f"File processed successfully | Total messages: {parse_file_result.count} | Time: {parse_file_result.duration}")
-        
-        response = parse_file_result
-        background_tasks.add_task(remove_temp_file, temp_file_path)
-
-        return response
+        return StreamingResponse(
+            stream_parsed_objects(manager, temp_file_path), 
+            media_type="application/x-jsonlines"
+        )
     
     except Exception as e:
         logger.error(f"Server failed: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="server failed")
-
-
+    
 @router.get("/health")
 async def health_check():
     return {"status": "healthy"}
